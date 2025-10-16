106a399a79c870bcf7ded3ac2e71b813c636079a
Adle24 <askaradilet41@gmail.com>
2025-10-16 20:11:33 +0500

    finished chapter 11

---
+++ cryptids/src/auth.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+
+app = FastAPI()
+
+secret_user: str = "newphone"
+secret_password: str = "whodis?"
+
+basic = HTTPBasic()
+
+
+@app.get("/who")
+def get_user(creds: HTTPBasicCredentials = Depends(basic)) -> dict:
+    if creds.username == secret_user and creds.password == secret_password:
+        return {"username": creds.username, "password": creds.password}
+
+    raise HTTPException(status_code=401, detail="Incorrect username or password")
---
+++ cryptids/src/web/user.py
@@ -0,0 +1,77 @@
+import os
+from fastapi import APIRouter, HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from ..model.user import User
+from datetime import timedelta
+
+if os.getenv("CRYPTID_UNIT_TEST"):
+    from ..fake import user as service
+else:
+    from ..service import user as service
+
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+router = APIRouter(prefix="/user")
+oauth2_dep = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def unauthed():
+    raise HTTPException(
+        status_code=401,
+        detail="Incorrect username or password",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+
+@router.post("/token")
+async def create_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = service.auth_user(form_data.username, form_data.password)
+    if not user:
+        unauthed()
+    expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = service.create_access_token(
+        data={"sub": user.username}, expires=expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/token")
+def get_access_token(token: str = Depends(oauth2_dep)) -> dict:
+    return {"token": token}
+
+
+@router.get("/")
+def get_all() -> list[User]:
+    return service.get_all()
+
+
+@router.get("/{name}")
+def get_one(name) -> User:
+    try:
+        return service.get_one(name)
+    except:
+        raise HTTPException(status_code=404, detail="unable to find user")
+
+
+@router.post("/", status_code=201)
+def create(user: User) -> User:
+    try:
+        return service.create(user)
+    except:
+        raise HTTPException(status_code=409, detail="unable to find user")
+
+
+@router.patch("/")
+def modify(name: str, user: User) -> User:
+    try:
+        return service.modify(name, user)
+    except:
+        raise HTTPException(status_code=404, detail="duplicate user found")
+
+
+@router.delete("/{name}")
+def delete(name: str) -> None:
+    try:
+        return service.delete(name)
+    except:
+        raise HTTPException(status_code=404, detail="user not found")
