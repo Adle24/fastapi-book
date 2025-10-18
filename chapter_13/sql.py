777aa85c67dad8d5559466bf81f687cef97a00c5
Adle24 <askaradilet41@gmail.com>
2025-10-18 16:27:32 +0500

    finished chapter 13

---
+++ chapter_13/sql.py
@@ -0,0 +1,15 @@
+from sqlalchemy import MetaData, Table, Column, Text
+from sqlalchemy import create_engine ,insert
+
+engine = create_engine('sqlite:///book.db')
+meta = MetaData()
+
+book_table = Table(
+    "book",
+    meta,
+    Column("name", Text, primary_key=True),
+    Column("author", Text),
+    Column("description", Text),
+)
+
+insert(book_table).values(name="Angels and Demons", author="Dan Brown", description="A book about Angels and Demons")
